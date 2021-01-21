af4afccb4b6c9d9df73ae88f3463b565ca2441d6
Paul Thomas <paulthomas2107@gmail.com>
2021-01-21 17:49:39 +0000

    Commit first

--- demo/src/main/java/com/example/demo/appuser/AppUser.java
+++ demo/src/main/java/com/example/demo/appuser/AppUser.java
@@ -27,23 +27,21 @@ public class AppUser implements UserDetails {
             generator = "student_sequence"
     )
     private Long id;
-    private String name;
-    private String username;
+    private String firstName;
+    private String lastName;
     private String email;
     private String password;
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
-    private Boolean locked;
-    private Boolean enabled;
+    private Boolean locked = false;
+    private Boolean enabled = false;
 
-    public AppUser(String name, String username, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
-        this.name = name;
-        this.username = username;
+    public AppUser(String firstName, String lastName, String email, String password, AppUserRole appUserRole) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.appUserRole = appUserRole;
-        this.locked = locked;
-        this.enabled = enabled;
     }
 
     @java.lang.Override
@@ -59,7 +57,15 @@ public class AppUser implements UserDetails {
 
     @java.lang.Override
     public java.lang.String getUsername() {
-        return username;
+        return email;
+    }
+
+    public java.lang.String getFirstName() {
+        return firstName;
+    }
+
+    public java.lang.String getLastName() {
+        return lastName;
     }
 
     @java.lang.Override
--- demo/src/main/java/com/example/demo/registration/RegistrationService.java
+++ demo/src/main/java/com/example/demo/registration/RegistrationService.java
@@ -1,11 +1,32 @@
 package com.example.demo.registration;
 
+import com.example.demo.appuser.AppUser;
+import com.example.demo.appuser.AppUserRole;
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
 
+    private final EmailValidator emailValidator;
+    private final AppUserService appUserService;
+
     public String register(RegistrationRequest request) {
-        return "WORKS....";
+        boolean isValidEmail = emailValidator.test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("Email is not valid");
+        }
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+
+                )
+        );
     }
 }
